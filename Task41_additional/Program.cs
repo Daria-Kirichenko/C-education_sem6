0043449574e24e880b4a3d026e26017a2c18afdb
Daria <darakirichenko@gmail.com>
2023-03-04 21:55:44 +0300

    Additional task is done

---
+++ Task41_additional/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры неопределенное количество чисел (while (true)). 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода слова "stop"
+
+int count = 0;
+string stop = "stop";
+Console.WriteLine("Введите число:");
+while (true)
+{
+    string userAnswer = Console.ReadLine()!;
+    if(userAnswer.ToLower() == stop)
+    {
+        break;
+    }
+    int number = Convert.ToInt32(userAnswer);
+    if(number > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел = {count}");
