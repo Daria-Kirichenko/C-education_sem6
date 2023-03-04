bc273236557aca7ab0ac62dbb89367b625d9fcb6
Daria <darakirichenko@gmail.com>
2023-03-04 19:55:22 +0300

    Task41 is done

---
+++ Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadNumber("Введите количество чисел в массиве:");
+
+int[] GetArray()
+{
+    int[] array = new int[number];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadNumber("Введите число:");
+    }
+    return array;
+}
+
+int CountOfPositiveElements(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > i)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] userArray = GetArray();
+int countPos = CountOfPositiveElements(userArray);
+Console.WriteLine($"[{string.Join(", ", userArray)}]");
+Console.WriteLine($"Количество положительных чисел = {countPos}");
